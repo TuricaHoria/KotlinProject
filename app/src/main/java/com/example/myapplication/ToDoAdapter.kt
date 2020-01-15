831e6827ebf8fbe69e67cad98ad71bc8530c0c82
user <horia.turica@cst-dev.com>
2020-01-15 11:40:42 +0200

    Created adapter for the to do RecyclerView

--- app/src/main/java/com/example/myapplication/ToDoAdapter.kt
+++ app/src/main/java/com/example/myapplication/ToDoAdapter.kt
@@ -6,30 +6,31 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ToDoAdapter (val toDoList : ArrayList<ToDo>) : RecyclerView.Adapter<ToDoAdapter.ViewHolder>()
+class ToDoAdapter (val toDos: ArrayList<ToDo>) : RecyclerView.Adapter<ToDoAdapter.ViewHolder>()
 {
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
-        val toDo : ToDo = toDoList[position]
-        holder.titleTextView.text = toDo.title
-        holder.completedTextView.text = toDo.completed.toString()
-
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.to_do_fragment,parent,false)
         return ViewHolder(v)
     }
 
     override fun getItemCount(): Int {
-        return toDoList.size
+        return toDos.size
+    }
+
+    override fun onBindViewHolder(holder:ViewHolder, position: Int) {
+        val toDo : ToDo = toDos[position]
+
+        holder.toDoTitleTV.text = toDo.title
+
+        if(toDo.completed)
+        holder.toDoCompletedTV.text = "Completed"
     }
 
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
-        val titleTextView = itemView.findViewById(R.id.title) as TextView
-        val completedTextView = itemView.findViewById(R.id.completed) as TextView
+        val toDoTitleTV = itemView.findViewById(R.id.title) as TextView
+        val toDoCompletedTV = itemView.findViewById(R.id.completed) as TextView
     }
 
 }
